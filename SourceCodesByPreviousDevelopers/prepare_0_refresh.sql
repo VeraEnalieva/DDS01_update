9897368e43dc2435db3bdcb12793b0b5e0d1c037
Еналиева Вера Валерьевна <v.enalieva@office.sspk.spb.ru>
2023-12-11 17:03:19 +0300

    Start working

--- SourceCodesByPreviousDevelopers/prepare_0_refresh.sql
+++
@@ -1,8 +0,0 @@
-delete from public.planet_osm_line where true;
-insert into public.planet_osm_line (select * from public.planet_osm_line_dc);
-delete from public.planet_osm_point where true;
-insert into public.planet_osm_point (select * from public.planet_osm_point_dc);
-delete from public.planet_osm_polygon where true;
-insert into public.planet_osm_polygon (select * from public.planet_osm_polygon_dc);
-delete from public.planet_osm_roads where true;
-insert into public.planet_osm_roads (select * from public.planet_osm_roads_dc);
